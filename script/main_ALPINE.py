40baeaeabad7c033417ec46e8e4bbd21be72053d
elipizz <elipizz@gmail.com>
2021-04-10 18:31:51 +0200

    trying also the wo halo objects

--- script/main_ALPINE.py
+++ script/main_ALPINE.py
@@ -87,8 +87,9 @@ data_counter = 0
 
 for data in datas:
     
-    if data.params_obs["name"] not in names_wo_CII_halo: #or data.params_obs["name"] != "DEIMOS_COSMOS_881725":
-    #if data.params_obs["name"] != "DEIMOS_COSMOS_351640":
+    #if data.params_obs["name"] not in names_wo_CII_halo: #or data.params_obs["name"] != "DEIMOS_COSMOS_881725":
+    if data.params_obs["name"] in names_wo_CII_halo or data.params_obs["name"] not in names_CII_halo:#names_wo_CII_halodata.params_obs["name"] != "DEIMOS_COSMOS_881725":
+    #if data.params_obs["name"] != "DEIMOS_COSMOS_733857":
         pass
     else:
         print("#####################")
