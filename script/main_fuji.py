3eb910f2521d3a4c750456e47f64e0a867a92ad7
elipizz <elipizz@gmail.com>
2021-04-13 16:44:03 +0200

    running more fuji

--- script/main_fuji.py
+++ script/main_fuji.py
@@ -76,12 +76,12 @@ plot_emission = False
 
 plot_eta = False
 
-fesc = 0.0
+fesc = 0.2
 
 betas = np.asarray([1.3,1.4,1.5,1.6,1.7,1.8,1.9,2.0,2.1,2.2,2.3,2.4,2.5,2.6,2.7,2.8,2.9,3.0,3.1,3.2,3.3,3.4,3.5,3.6])
-#betas = np.asarray([1.8,2.0,2.2,2.4,2.6,2.8,3.0,3.2])
 #betas = [3.0]
 betas = np.asarray([3.7,3.8,3.9,4.0,4.1,4.2,4.3,4.4,4.5])
+betas = np.asarray([1.8,2.0,2.2,2.4,2.6,2.8,3.0,3.2])
 
 data = observational_data_fuji
 
