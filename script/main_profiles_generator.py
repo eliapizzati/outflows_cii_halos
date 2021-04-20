cd1ab82a969ebb7c5678cf744a47176816d26bda
elipizz <elipizz@gmail.com>
2021-04-20 17:44:29 +0200

    adding profiles generator

---
+++ script/main_profiles_generator.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 20 16:39:49 2021
+
+@author: anna
+"""
+
+
+
+import os
+import numpy as np
+
+
+from load_data import obs_data_list, names, names_CII_halo, names_wo_CII_halo, names_other,  observational_data_fuji
+from sol_modules import get_profiles
+            
+
+import time
+
+
+verbose = True
+
+v_cs = []
+redshifts = []
+SFRs = [] 
+
+for data in obs_data_list:
+    v_cs.append(data.params_obs["v_c"])
+    redshifts.append(data.params_obs["redshift"])
+    SFRs.append(data.params_obs["SFR"])
+    
+f_esc_ion = 0.0
+
+f_esc_FUVs = [0.0,0.05,0.1,0.2]
+
+betas = np.linspace(1.0,4.0, 31)
+
+#v_cs = np.linspace(175.,300., 26)
+
+params = dict([("DM_model", "NFW"),
+           ("beta", 1.0), 
+           ("SFR", ),
+           ("f_esc_ion", f_esc_ion), 
+           ("f_esc_FUV", 0.0), 
+           ("v_c", 175.),
+           ("redshift", 5.),
+           ("Zeta", 1.0),
+           ("alfa", 1.0),
+           ("R_in", 0.3)])    
+
+    
+
+
+
+for v_c, SFR, z, counter_data in zip(v_cs, SFRs, redshifts, range(len(obs_data_list))):
+    
+    if verbose:
+          print("#################################")
+          print("updating  obs parameters (v_c, SFR, z), number {}/{}".format(counter_data, len(obs_data_list)))
+          print("#################################")  
+    
+    for f_esc_FUV, counter_fesc in zip(f_esc_FUVs, range(len(f_esc_FUVs))):
+    
+        if verbose:
+                print("#################################")
+                print("updating  f_esc_FUV, number {}/{}".format(counter_fesc, len(f_esc_FUVs)))
+                print("#################################")
+           
+        for beta in betas:
+            
+            time_start = time.perf_counter()
+
+            params.update(beta = beta)
+            params.update(f_esc_FUV = f_esc_FUV)
+            
+            if verbose:
+                print("run with beta = {:.1f}, f_esc_FUV = {:.2f}, v_c = {:.1f}, SFR = {:.1f}, z = {:.1f}".format(\
+                      beta, f_esc_FUV, v_c, SFR, z))
+            
+            profiles = get_profiles(params, resol=1000)
+        
+            profiles.to_file()
+                
+            if profiles.check_nans() == True:
+                string_nans = "Integration error: presence of nans"
+            else:
+                string_nans = "Integration successful"
+                    
+            print("beta=", beta, "\t", string_nans)
+            
+            time_elapsed = (time.perf_counter() - time_start)
+        
+            print("total time elapsed (s)=", time_elapsed)
+
+
+
+
