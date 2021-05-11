3223f52f20b171da8a735816ebdb84a4888146d0
elipizz <elipizz@gmail.com>
2021-05-11 14:15:21 +0200

    fast solver added

--- script/fast_solver.py
+++ script/fast_solver.py
@@ -123,7 +123,7 @@ def diff_system_fast(r, y, SFR_pure, redshift, M_vir_pure, f_esc_ion, f_esc_FUV,
     return output
 
 
-def stopping_condition(t, y): 
+def stopping_condition(t, y, SFR_pure, redshift, M_vir_pure, f_esc_ion, f_esc_FUV, Plw, Ph1, Pg1, Zeta): 
     return y[0] - 50. # in km/s
 
 stopping_condition.terminal = True
