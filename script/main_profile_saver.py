4140212b6d68a0d652380bb1296f84c13ab9e1f0
eliapizzati <elia.pizzati@sns.it>
2022-11-23 16:45:21 +0100

    finishing files per laura 2

--- script/main_profile_saver.py
+++ script/main_profile_saver.py
@@ -24,11 +24,11 @@ plotting = True
 
 
 params = dict([("DM_model", "NFW+disk"),
-               ("beta", 5.5),
-               ("SFR", 32.),
+               ("beta", 4.3),
+               ("SFR", 105.),
                ("f_esc_ion", 0.0),
                ("f_esc_FUV", 0.0),
-               ("v_c", 197.),
+               ("v_c", 217.),
                ("redshift", 5.0),
                ("Zeta", 1.0),
                ("alfa", 1.0),
@@ -58,7 +58,7 @@ else:
     string_nans = "Integration successful"
 print(string_nans)
 
-profiles.to_file(attributes_in_name=f"DC880016_{resol}_per_laura_log_grid")
+profiles.to_file(attributes_in_name=f"DC881725_{resol}_per_laura_log_grid")
 
 if post_profiles:
     # getting the ionization states, the sigma CII and the convolved intensity for the CII
